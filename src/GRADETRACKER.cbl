5753ecb41501fdd1396111a534fb76918e73b073
AhyaanMalik <ak27mali@siena.edu>
2026-05-07 23:02:11 -0400

    feat: add variables and user input

--- src/GRADETRACKER.cbl
+++ src/GRADETRACKER.cbl
@@ -6,9 +6,17 @@
 
        DATA DIVISION. 
        WORKING-STORAGE SECTION. 
+       01 WS-STUDENT-NAME PIC X(30).
+       01 WS-SCORE PIC 999.
 
        PROCEDURE DIVISION.
        MAIN-PARA.
            DISPLAY 'Grade Tracker'
+           DISPLAY '------------'
+           DISPLAY 'Enter Student Name: '
+           ACCEPT WS-STUDENT-NAME
+           DISPLAY 'Enter Score (0-100): '
+           ACCEPT WS-SCORE
+           DISPLAY 'Student: ' WS-STUDENT-NAME
+           DISPLAY 'Score: ' WS-SCORE 
            STOP RUN.
-           
